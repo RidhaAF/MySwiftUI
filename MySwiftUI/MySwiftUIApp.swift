307423acd1caae8346837242dc5ba2d7ae08b5e1
ridhaaf <ridhaaf@gmail.com>
2022-07-21 22:42:19 +0700

    Initial Commit

---
+++ MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by Ridha Ahmad Firdaus on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
